ea018ed7285ff437ddec539f03a5313fd3b3b58d
Raifc <raif.si@hotmail.com>
2023-10-26 20:15:22 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(ExpensesApp());  
+void main() {
+  runApp(ExpensesApp());
+}
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text('Expenses'),
-    ),
-    body: Center(child: 'Initial version')
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expenses'),
+      ),
+      body: Center(
+        child: Text('Initial version'), // Use Text widget for displaying text
+      ),
+    );
   }
 }

37615fedb3e33199d912ef86582179c4298466d8
Raifc <raif.si@hotmail.com>
2023-11-02 17:21:46 -0300

    Updates list container height

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
+      height: 700,
       child: transactions.isEmpty
           ? Column(
               children: [

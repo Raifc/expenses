c6d8577d4de301f599bdea3f4b600d0ed09c64a0
Raifc <raif.si@hotmail.com>
2023-11-13 23:45:52 -0300

    creates adaptative button

---
+++ lib/components/adaptative_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class AdaptativeButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const AdaptativeButton(this.label, this.onPressed, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(label),
+            onPressed: onPressed,
+          )
+        : ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor:
+                  Theme.of(context).primaryColor, // Background color
+              foregroundColor:
+                  Theme.of(context).textTheme.labelLarge?.color, // Text color
+            ),
+            onPressed: onPressed,
+            child: Text(label),
+          );
+  }
+}

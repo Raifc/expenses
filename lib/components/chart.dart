c760c26a6e4647f5f4b0b8425db2827d0d47c81a
Raifc <raif.si@hotmail.com>
2023-11-15 19:18:40 -0300

    style fixes

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -9,27 +9,30 @@ class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
   List<Map<String, dynamic>> get groupedTransactions {
-    return List.generate(7, (index) {
-      final weekDay = DateTime.now().subtract(
-        Duration(days: index),
-      );
+    return List.generate(
+      7,
+      (index) {
+        final weekDay = DateTime.now().subtract(
+          Duration(days: index),
+        );
 
-      double totalSum = 0.0;
-      for (var i = 0; i < recentTransactions.length; i++) {
-        bool sameDay = recentTransactions[i].date.day == weekDay.day;
-        bool sameMonth = recentTransactions[i].date.month == weekDay.month;
-        bool sameYear = recentTransactions[i].date.year == weekDay.year;
+        double totalSum = 0.0;
+        for (var i = 0; i < recentTransactions.length; i++) {
+          bool sameDay = recentTransactions[i].date.day == weekDay.day;
+          bool sameMonth = recentTransactions[i].date.month == weekDay.month;
+          bool sameYear = recentTransactions[i].date.year == weekDay.year;
 
-        if (sameDay && sameMonth && sameYear) {
-          totalSum += recentTransactions[i].value;
+          if (sameDay && sameMonth && sameYear) {
+            totalSum += recentTransactions[i].value;
+          }
         }
-      }
 
-      return {
-        'day': DateFormat.E().format(weekDay)[0],
-        'value': totalSum,
-      };
-    }).reversed.toList();
+        return {
+          'day': DateFormat.E().format(weekDay)[0],
+          'value': totalSum,
+        };
+      },
+    ).reversed.toList();
   }
 
   double get _weekTotalValue {
